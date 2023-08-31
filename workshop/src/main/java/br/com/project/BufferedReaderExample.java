aaca41b2eaceee22c321ca24626adee4f671e43d
Lucas Lima <lucas.lima@compasso.com.br>
2023-08-31 16:43:37 -0300

    Exemplo utilizando o BufferedReader, alternativa para o Sanner

---
+++ workshop/src/main/java/br/com/project/BufferedReaderExample.java
@@ -0,0 +1,33 @@
+package br.com.project;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderExample {
+
+
+    public static void main(String[] args) {
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Escreva um número de 0 a 10: ");
+
+        try{
+            var input = reader.readLine();
+            var valueInput = Integer.valueOf(input);
+
+            validateInput(valueInput);
+
+            System.out.println("O número que você colocou é: " + valueInput);
+
+        }catch (IOException e){
+            throw new RuntimeException("Erro na leitura");
+        }
+    }
+
+    private static void validateInput(Integer value){
+        if(value > 10 || value < 0){
+            throw new RuntimeException("O valor que você colocou é maior que 10 ou menor que 0");
+        }
+    }
+}
